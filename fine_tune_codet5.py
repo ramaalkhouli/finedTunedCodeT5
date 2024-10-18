bd7095149393a36416ecdbd8ef69e28ee9e6af3f
ramaalkhouli <ramaalkhouli19@gmail.com>
2024-10-18 12:08:09 -0400

    modified training script and added job script

--- fine_tune_codet5.py
+++ fine_tune_codet5.py
@@ -14,7 +14,6 @@ model.to(device)
 # Load dataset
 dataset = load_dataset('code_x_glue_ct_code_to_text', 'python')
 
-
 # Preprocessing function
 def preprocess_function(examples):
     inputs = examples['code']
@@ -24,20 +23,19 @@ def preprocess_function(examples):
     model_inputs['labels'] = labels['input_ids']
     return model_inputs
 
-
 # Tokenize the dataset
 tokenized_datasets = dataset.map(preprocess_function, batched=True)
 
-# Set up training arguments for Colab (uses GPU if available)
+# Set up training arguments for Compute Canada (using GPU if available)
 training_args = TrainingArguments(
     output_dir='./results',
     num_train_epochs=3,
-    per_device_train_batch_size=4,
-    per_device_eval_batch_size=4,
+    per_device_train_batch_size=8,  # Increase batch size for V100 GPUs
+    per_device_eval_batch_size=8,
     warmup_steps=500,
     weight_decay=0.01,
     logging_dir='./logs',
-    report_to="none"  # Disabling reporting to prevent warnings
+    report_to="none"
 )
 
 # Initialize the Trainer
